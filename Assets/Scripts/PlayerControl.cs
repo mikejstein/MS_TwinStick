ff3badadb8b755f135da57e5ee89c3536f67656a
mikejstein <mike.j.stein@gmail.com>
2016-10-30 15:48:50 -0700

    Initial Commit
    first steps

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed = 10; // How fast we move around
+    public float turnSpeed = 50; // How fast we turn around
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        PlayerInput.inputPolled += MoveAndShoot; //make sure we're subscribed to the event out of Player Input
+
+        rb = GetComponent<Rigidbody>(); // Connect our rigidibody variable to our rigidbody component
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void MoveAndShoot(float leftX, float leftY, float rightX, float rightY)
+    {
+        /* Calculate our movement vector.
+         * We first create a Vector3 from our lefX and leftY parameteres.
+         * Then we convert that from local space to world space.
+         * Then we multiplly that by our movement units and time since we last ran this function
+         */
+        Vector3 movement = transform.TransformDirection(new Vector3(leftX, 0, leftY) * moveSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + movement);
+
+        /* Calculate our shoot vector.
+         * For this, we actually 
+         */
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Class for handling user input. We send the data over to PlayerControl for figuring out what to do with the input.
+ * This means if we need to change input schemas, I can do that cleanly
+ */
+public class PlayerInput : MonoBehaviour {
+    /*
+     * An event that is fired every time we check for input.
+     * It sends the data back to whomever is subscribed to the event.
+     */ 
+    public delegate void InputPolled(float leftX, float leftY, float rightX, float rightY);
+    public static event InputPolled inputPolled;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame.
+    // We poll the left and right sticks, and then call 
+	void Update () {
+        float lStickX = Input.GetAxis("Horizontal");
+        float lStickY = Input.GetAxis("Vertical");
+
+        float rStickX = Input.GetAxis("Right Stick X");
+        float rStickY = Input.GetAxis("Right Stick Y");
+
+        inputPolled(lStickX, lStickY, rStickX, rStickY);
+    }
+}
